b3ec6a45c116e9d5c7537d353c7ecbc9913a879a
_manuel_ <manue.l@live.com.ar>
2010-11-04 23:17:45 -0300

    Updates to Anticheat System:
    * Now you are able to check the average of reports (report/time) made while he has been online since last login. (Huge thanks to Gyullo)
    
    *Implemented new commands and new way for control cheaters:
        * .anticheat global: returns the top three cheat reports amount, and the top three with highest average.
        * .anticheat delete: deletes a especific player' reports or all reports from tables.
        * .anticheat handle: turns off/on the anticheat system.
        * .anticheat player: returns the amount and average of a especific player.
    
    * Implemented speed-hack detection while player is underwater.
    * Implemented fly-hack detection while player is on vehicle.
    
    * False reports casued because of lag problems SHOULD BE SOLVED, now we use the movement packet time value instead of server-side time, for calculations.
    
    Patch for TrinityCore rev 10359 (a168ad157489).

---
+++ sqls/2_anticheat.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS cheat_reports;
+CREATE TABLE cheat_reports (
+  `id` int(11) unsigned NOT NULL auto_increment ,
+  `guid` float(11) unsigned NOT NULL,
+  `name` text,
+  `mapid` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `position_x` float NOT NULL DEFAULT '0',
+  `position_y` float NOT NULL DEFAULT '0',
+  `position_z` float NOT NULL DEFAULT '0',
+  `report` TEXT,
+  `time` BIGINT(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS cheat_temp_reports;
+CREATE TABLE cheat_temp_reports (
+  `id` int(11) unsigned NOT NULL auto_increment ,
+  `guid` float(11) unsigned NOT NULL,
+  `name` text,
+  `mapid` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `position_x` float NOT NULL DEFAULT '0',
+  `position_y` float NOT NULL DEFAULT '0',
+  `position_z` float NOT NULL DEFAULT '0',
+  `report` TEXT,
+  `time` BIGINT(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (id)
+);
